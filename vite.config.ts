6a3e4a9257a1dc441d686a9927ccfa7384de2520
Hassan Tahir <hassan.tahir@hotmail.com>
2025-02-28 18:27:04 +0500

    About Contact Footer try 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/m-hassantahir.github.io/', // Important for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
